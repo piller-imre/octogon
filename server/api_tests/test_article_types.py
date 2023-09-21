17e5b83be88fc23afbe504aba3304d8c1301bd85
Imre Piller <piller.imre@gmail.com>
2023-09-21 05:45:55 +0200

    Article related API test cases

--- server/api_tests/test_article_types.py
+++ server/api_tests/test_article_types.py
@@ -19,65 +19,229 @@ class ArticleTypesTest(unittest.TestCase):
             # TODO: Perform a login and use the token in the header!
             resp = requests.post('http://127.0.0.1:80/api/article-types/collect', json=req)
             self.assertEqual(200, resp.status_code)
-            self.assertEqual([], resp.json())
+            self.assertEqual({'articleTypes': []}, resp.json())
 
     def test_collect_multiple(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {}
+            resp = requests.post('http://127.0.0.1:80/api/article-types/collect', json=req)
+            self.assertEqual(200, resp.status_code)
+            article_types = resp.json()['articleTypes']
+            self.assertEqual(3, len(article_types))
+            self.assertEqual({
+                'id': 1,
+                'name': 'research',
+                'description': 'Research paper'
+            }, article_types[0])
+            self.assertEqual({
+                'id': 2,
+                'name': 'competition',
+                'description': 'Mathematical competition'
+            }, article_types[1])
+            self.assertEqual({
+                'id': 3,
+                'name': 'proposed problems',
+                'description': 'Some proposed problems'
+            }, article_types[2])
 
     def test_get_successful(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {'id': 2}
+            resp = requests.post('http://127.0.0.1:80/api/article-types/get', json=req)
+            self.assertEqual(200, resp.status_code)
+            article_type = resp.json()
+            self.assertEqual({
+                'id': 2,
+                'name': 'competition',
+                'description': 'Mathematical competition'
+            }, article_type)
 
     def test_get_invalid_request(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {'other': 2}
+            resp = requests.post('http://127.0.0.1:80/api/article-types/get', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_get_missing_id(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {'id': 4}
+            resp = requests.post('http://127.0.0.1:80/api/article-types/get', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_create_unauthenticated(self):
         ...
 
     def test_create_successful(self):
-        ...
+        with prepare_test_context('empty'):
+            req = {
+                'name': 'competition',
+                'description': 'Mathematical competition'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/create', json=req)
+            self.assertEqual(200, resp.status_code)
+            self.assertEqual({'id': 1}, resp.json())
 
     def test_create_invalid_request(self):
-        ...
+        with prepare_test_context('empty'):
+            req = {
+                'name': 'competition'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/create', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_create_missing_name(self):
-        ...
+        with prepare_test_context('empty'):
+            req = {
+                'name': '',
+                'description': 'Mathematical competition'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/create', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_create_existing_name(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'name': 'competition',
+                'description': 'Mathematical competition'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/create', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_update_unauthenticated(self):
         ...
 
     def test_update_successful(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 3,
+                'name': 'race',
+                'description': 'Mathematical race'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/update', json=req)
+            self.assertEqual(200, resp.status_code)
+            req = {'id': 3}
+            resp = requests.post('http://127.0.0.1:80/api/article-types/get', json=req)
+            self.assertEqual(200, resp.status_code)
+            article_type = resp.json()
+            self.assertEqual({
+                'id': 3,
+                'name': 'race',
+                'description': 'Mathematical race'
+            }, article_type)
 
     def test_update_invalid_request(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 3,
+                'name': 'race'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/update', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_update_missing_type_id(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 4,
+                'name': 'race',
+                'description': 'Mathematical race'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/update', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_update_missing_name(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 3,
+                'name': '',
+                'description': 'Mathematical race'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/update', json=req)
+            self.assertEqual(400, resp.status_code)
+
+    def test_update_same_name(self):
+        with prepare_test_context('demo'):
+            req = {
+                'id': 3,
+                'name': 'proposed problems',
+                'description': 'Something new'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/update', json=req)
+            self.assertEqual(200, resp.status_code)
+            req = {'id': 3}
+            resp = requests.post('http://127.0.0.1:80/api/article-types/get', json=req)
+            self.assertEqual(200, resp.status_code)
+            article_type = resp.json()
+            self.assertEqual({
+                'id': 3,
+                'name': 'proposed problems',
+                'description': 'Something new'
+            }, article_type)
 
     def test_update_existing_name(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 3,
+                'name': 'research',
+                'description': 'Mathematical race'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/update', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_remove_unauthenticated(self):
         ...
 
     def test_remove_successful(self):
-        ...
+        with prepare_test_context('empty'):
+            n = 10
+            for i in range(1, n + 1):
+                req = {
+                    'name': f'competition {n}',
+                    'description': f'Mathematical competition {n}'
+                }
+                resp = requests.post('http://127.0.0.1:80/api/article-types/create', json=req)
+                self.assertEqual(200, resp.status_code)
+            req = {}
+            resp = requests.post('http://127.0.0.1:80/api/article-types/collect', json=req)
+            self.assertEqual(200, resp.status_code)
+            article_types = resp.json()['articleTypes']
+            self.assertEqual(10, len(article_types))
+            req = {
+                'id': 5
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/remove', json=req)
+            req = {}
+            resp = requests.post('http://127.0.0.1:80/api/article-types/collect', json=req)
+            self.assertEqual(200, resp.status_code)
+            article_types = resp.json()['articleTypes']
+            self.assertEqual(9, len(article_types))
+            remaining_ids = [article_type['id'] for article_type in article_types]
+            self.assertEqual([1, 2, 3, 4, 6, 7, 8, 9, 10], remaining_ids)
 
     def test_remove_invalid_request(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'invalid': 2
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/remove', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_remove_missing_id(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 4
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/remove', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_remove_in_use(self):
-        ...
-
+        with prepare_test_context('demo'):
+            req = {
+                'id': 1
+            }
+            resp = requests.post('http://127.0.0.1:80/api/article-types/remove', json=req)
+            self.assertEqual(400, resp.status_code)
+            req = {}
+            resp = requests.post('http://127.0.0.1:80/api/article-types/collect', json=req)
+            self.assertEqual(200, resp.status_code)
+            article_types = resp.json()['articleTypes']
+            self.assertEqual(3, len(article_types))
--- server/api_tests/test_articles.py
+++ server/api_tests/test_articles.py
@@ -11,81 +11,394 @@ import requests
 class ArticleTest(unittest.TestCase):
 
     def test_collect_successful(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'volumeId': 2
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/collect', json=req)
+            self.assertEqual(200, resp.status_code)
+            expected_response = {
+                'volume': {
+                    'id': 2,
+                    'volume': 18,
+                    'number': 2,
+                    'year': 2010,
+                    'month': 10,
+                    'isVisible': True
+                },
+                'articles': [
+                    {
+                        'id': 6,
+                        'articleType': 'research',
+                        'title': 'First in second volume',
+                        'authors': [
+                            {
+                                'givenName': 'Péter',
+                                'familyName': 'Körtesi',
+                                'affiliation': 'Hungary',
+                                'email': 'matkp@uni-miskolc.hu'
+                            },
+                            {
+                                'givenName': 'Mihály',
+                                'familyName': 'Bencze',
+                                'affiliation': 'Brasov',
+                                'email': 'benczemihaly@gmail.com'
+                            },
+                            {
+                                'givenName': 'Ovidiu T.',
+                                'familyName': 'Pop',
+                                'affiliation': 'Satu Mare, Romania',
+                                'email': 'pop@satumare.ro'
+                            }
+                        ],
+                        'articleFile': '',
+                        'firstPage': 1,
+                        'lastPage': 12
+                    },
+                    {
+                        'id': 7,
+                        'articleType': 'research',
+                        'title': 'Second in second volume',
+                        'authors': [
+                            {
+                                'givenName': 'Sever S.',
+                                'familyName': 'Dragomir',
+                                'affiliation': 'Australia',
+                                'email': 'dragomir@octogon.com'
+                            }
+                        ],
+                        'articleFile': '',
+                        'firstPage': 13,
+                        'lastPage': 19
+                    },
+                    {
+                        'id': 8,
+                        'articleType': 'proposed problems',
+                        'title': 'Proposed problems',
+                        'authors': [
+                            {
+                                'givenName': 'Mihály',
+                                'familyName': 'Bencze',
+                                'affiliation': 'Brasov',
+                                'email': 'benczemihaly@gmail.com'
+                            }
+                        ],
+                        'articleFile': 'PP_2010_2.pdf',
+                        'firstPage': 20,
+                        'lastPage': 33
+                    }
+                ]
+            }
+            self.assertEqual(expected_response, resp.json())
 
     def test_collect_invalid_request(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 2
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/collect', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_collect_invalid_volume_id(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'volumeId': 404
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/collect', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_get_successful(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 8
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/get', json=req)
+            self.assertEqual(200, resp.status_code)
+            expected_response = {
+                'volume': {
+                    'id': 2,
+                    'volume': 18,
+                    'number': 2,
+                    'year': 2010,
+                    'month': 10,
+                    'isVisible': True
+                },
+                'article': {
+                    'id': 8,
+                    'articleType': 'proposed problems',
+                    'title': 'Proposed problems',
+                    'abstract': 'Exciting problems',
+                    'authors': [
+                        {
+                            'givenName': 'Mihály',
+                            'familyName': 'Bencze',
+                            'affiliation': 'Brasov',
+                            'email': 'benczemihaly@gmail.com'
+                        }
+                    ],
+                    'articleFile': 'PP_2010_2.pdf',
+                    'firstPage': 20,
+                    'lastPage': 33
+                }
+            }
+            self.assertEqual(expected_response, resp.json())
 
     def test_get_invalid_request(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'articleId': 8
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/get', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_get_invalid_article_id(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 12
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/get', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_create_unauthenticated(self):
         ...
 
     def test_create_successful(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'volumeId': 3
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/create', json=req)
+            self.assertEqual(200, resp.status_code)
+            self.assertEqual({'id': 10}, resp.json())
 
     def test_create_invalid_request(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'volume': 3
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/create', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_create_invalid_volume_id(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'volumeId': 6
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/create', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_update_title_data_unauthenticated(self):
         ...
 
     def test_update_title_data_successful(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 3,
+                'articleType': 2,
+                'title': 'Modified Title',
+                'abstract': 'Altered abstract content'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/update-title-data', json=req)
+            self.assertEqual(200, resp.status_code)
+            req = {
+                'id': 3
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/get', json=req)
+            self.assertEqual(200, resp.status_code)
+            expected_response = {
+                'volume': {
+                    'id': 3,
+                    'volume': 18,
+                    'number': 1,
+                    'year': 2010,
+                    'month': 4,
+                    'isVisible': True
+                },
+                'article': {
+                    'id': 8,
+                    'articleType': 'competition',
+                    'title': 'Modified Title',
+                    'abstract': 'Altered abstract content',
+                    'authors': [
+                        {
+                            'givenName': 'Péter',
+                            'familyName': 'Körtesi',
+                            'affiliation': 'Hungary',
+                            'email': 'matkp@uni-miskolc.hu'
+                        }
+                    ],
+                    'articleFile': '',
+                    'firstPage': 16,
+                    'lastPage': 22
+                }
+            }
+            self.assertEqual(expected_response, resp.json())
 
     def test_update_title_data_invalid_request(self):
-        # TODO: Test with additional field.
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 3,
+                'articleType': 2,
+                'title': 'Modified Title',
+                'abstract': 'Altered abstract content',
+                'unnecessary': True
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/update-title-data', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_update_title_data_invalid_article_id(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 30,
+                'articleType': 2,
+                'title': 'Modified Title',
+                'abstract': 'Altered abstract content'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/update-title-data', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_update_title_data_missing_title(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 3,
+                'articleType': 2,
+                'title': ' ',
+                'abstract': 'Altered abstract content'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/update-title-data', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_update_title_data_missing_abstract(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 3,
+                'articleType': 2,
+                'title': 'Modified Title',
+                'abstract': ' '
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/update-title-data', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_update_page_range_unauthenticated(self):
         ...
 
     def test_update_page_range_successful(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 3,
+                'firstPage': 22,
+                'lastPage': 33
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/update-page-range', json=req)
+            self.assertEqual(200, resp.status_code)
+            req = {
+                'id': 3
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/get', json=req)
+            self.assertEqual(200, resp.status_code)
+            expected_response = {
+                'volume': {
+                    'id': 3,
+                    'volume': 18,
+                    'number': 1,
+                    'year': 2010,
+                    'month': 4,
+                    'isVisible': True
+                },
+                'article': {
+                    'id': 8,
+                    'articleType': 'competition',
+                    'title': 'Modified Title',
+                    'abstract': 'Altered abstract content',
+                    'authors': [
+                        {
+                            'givenName': 'Péter',
+                            'familyName': 'Körtesi',
+                            'affiliation': 'Hungary',
+                            'email': 'matkp@uni-miskolc.hu'
+                        }
+                    ],
+                    'articleFile': '',
+                    'firstPage': 22,
+                    'lastPage': 33
+                }
+            }
+            self.assertEqual(expected_response, resp.json())
 
     def test_update_page_range_invalid_request(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 3,
+                'firstPage': 22,
+                'lastPage': 33,
+                'further': 'field'
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/update-page-range', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_update_page_range_invalid_article_id(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 30,
+                'firstPage': 22,
+                'lastPage': 33
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/update-page-range', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_update_page_range_invalid_interval(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 3,
+                'firstPage': 33,
+                'lastPage': 22
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/update-page-range', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_update_page_range_overlapping_interval(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 3,
+                'firstPage': 14,
+                'lastPage': 20
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/update-page-range', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_remove_unauthenticated(self):
         ...
 
     def test_remove_successful(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 4
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/remove', json=req)
+            self.assertEqual(200, resp.status_code)
+            req = {
+                'volumeId': 1
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/collect', json=req)
+            self.assertEqual(200, resp.status_code)
+            articles = resp.json()['articles']
+            self.assertEqual(4, len(articles))
+            article_ids = [article['id'] for article in articles]
+            self.assertEqual([1, 2, 3, 5], article_ids)
 
     def test_remove_invalid_request(self):
-        ...
+        with prepare_test_context('demo'):
+            req = {
+                'id': 4,
+                'other': ''
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/remove', json=req)
+            self.assertEqual(400, resp.status_code)
 
     def test_remove_invalid_article_id(self):
-        ...
-
+        with prepare_test_context('demo'):
+            req = {
+                'id': 404
+            }
+            resp = requests.post('http://127.0.0.1:80/api/articles/remove', json=req)
+            self.assertEqual(400, resp.status_code)
